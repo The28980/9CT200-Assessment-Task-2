be259146be5fc6ab132eb61542a89dde727c14f0
The28980 <160200971+The28980@users.noreply.github.com>
2024-08-13 10:50:25 +1000

    Update: Basic Menu Completion
    This update finishes the basic menu. The menu consists of an option to see the original dataset, search for more specific datasets and quit the program. In previous tests, the 'search for ore specific' option did not have further options stopping the user from continuing further. In this update, I have added further options within the option, however those options are yet to do anything and will be added later on.

--- main.py
+++ main.py
@@ -14,35 +14,64 @@ def ShowOriginalData():
 
 def MainMenu():
     global getout
-    print("""Welcome User to:
-    Top Songs of 2024: Data Analyser
-    
+    print("""
     Menu:
     1 - Load original dataset
     2 - Search for something more specific
     3 - Quit Program
-    Please enter the number corresponding to your command
-          """)
+    Please enter the number corresponding to your command""")
     
     try:
         command = int(input('-> '))
+        print("")
+       
         if command == 1:
+            print("Displaying Original Data...")
+            time.sleep(1)
             ShowOriginalData()
             print("Menu will re-appear in 10 seconds")
             time.sleep(10)
+        
         elif command == 2:
-            print("This is filler. Area not completed yet!")
+            print("""What would you like to search for?
+    1 - Artist
+    2 - Album
+    3 - Return to main menu""")
+            specify = int(input("-> "))
+            if specify == 1:
+                time.sleep(0.5)
+                print("")
+                print("Didn't think you'd make it this far (W.I.P)")
+                time.sleep(0.5)
+            elif specify == 2:
+                time.sleep(0.5)
+                print("")
+                print("Go back go back go back (W.I.P)")
+                time.sleep(0.5)
+            elif specify == 3:
+                print("Returning to main menu...")
+                time.sleep(1.5)
+
+        
         elif command == 3:
             print("Closing down program...")
             time.sleep(3)
             getout = True
+        
         else:
-            print("""Don't act like you can't count to 3 on one hand. Try again user.
-                  """)
+            time.sleep(0.5)
+            print("""
+Don't act like you can't count to 3 on one hand. Try again user.""")
+            time.sleep(2.5)
     except:
-        print("""Number. N-U-M-B-E-R. I don't even KNOW what this is meant to be. Try again user.
-              """)
+        time.sleep(0.5)
+        print("""
+Number. N-U-M-B-E-R. I don't even KNOW what this is meant to be. Try again user.""")
+        time.sleep(2.5)
 
 # +=================================================={ The Actual Code }==================================================+
+print("""
+Initiating 2024 Music Analysis System. Welcome User.""")
+time.sleep(2)
 while not getout:
     MainMenu()
